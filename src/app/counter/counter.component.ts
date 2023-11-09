bca278c7d25dce44bcdeec0a1a2d5c4c7d1bb775
Ke <kwang97@slb.com>
2023-11-09 16:58:19 +0800

    feat: implement data communication from counter output to couter-group

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'counter',
@@ -7,12 +7,17 @@ import { Component, Input } from '@angular/core';
 })
 export class CounterComponent {
   @Input() number = 0;
+  @Output() change = new EventEmitter();
+
 
   onIncreaseNumber(){
     this.number ++;
+    this.change.emit(this.number);
   }
 
   onDecreaseNumber(){
     this.number --;
+    this.change.emit(this.number);
   }
+  
 }
