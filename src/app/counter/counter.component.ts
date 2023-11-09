a41941d5062fc0c3a08e217285024df8e812daee
Shuhan Jin <sjin9@slb.com>
2023-11-09 16:40:20 +0800

    input data from counter-group( parent) component to counter(child) component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
-  number = 0
-
+  @Input() number = 0 //number被定义为可传递的属性
+  //用@Input()把父组件counter list的值传进子组件，实现父到子的数据传递
   onIncrease() {
     this.number++
   }
