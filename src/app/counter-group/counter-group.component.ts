41bcc5599c333e70a072fabd0056789180dcfd35
Shuhan Jin <sjin9@slb.com>
2023-11-09 17:17:20 +0800

    feat: emit data from child component, implemnt add and reset button in counter-group

--- src/app/counter-group/counter-group.component.ts
+++ src/app/counter-group/counter-group.component.ts
@@ -6,11 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter-group.component.css']
 })
 export class CounterGroupComponent {
-  counters: {num:number}[] = [ //prepare a list
-  //父子组件还未传递的时候countergroup里面的变量影响不了counter的值
-    {num:1},
-    {num:2},
-    {num:3}
-  ] 
+  counters: { num: number }[] = [ //prepare a list
+    //父子组件还未传递的时候countergroup里面的变量影响不了counter的值
+    { num: 1 },
+    { num: 2 },
+    { num: 3 }
+  ]
+  get sum() { //get将sum定义成属性(attribute)，不是method
+    return this.counters.reduce(
+      (result, current) => result + current.num, 0
+    ) //0 here is the initial value of sum, then +num1=result, +num2=result to get the sum of all items in a list
+  }
+
+  onAdd() {
+    this.counters.push({ num: 0 }) //.push() will add an item in list
+  }
+
+  onRemove() {
+    //this.counters.
+  }
+
+  resetSum() {
+    this.counters = []
+  }
 
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,12 +8,18 @@ import { Component, Input } from '@angular/core';
 export class CounterComponent {
   @Input() number = 0 //number被定义为可传递的属性
   //用@Input()把父组件counter list的值传进子组件，实现父到子的数据传递
+
+  @Output() change = new EventEmitter() //子向父传事件，固定写法
+  //change在这里只是一个变量名，在父组件html中调用同样变量名
+
   onIncrease() {
     this.number++
+    this.change.emit(this.number) //可以用$event在父组件html中调用这个值
   }
 
   onDecrease() {
     this.number--
+    this.change.emit(this.number)
   }
 
 }
