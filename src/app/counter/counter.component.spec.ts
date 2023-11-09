a2bb4f0651188ac0379b5d94b7d706c838779ec6
Wang Jing <jwang315@slb.com>
2023-11-09 16:20:46 +0800

    feat: generate counter component

---
+++ src/app/counter/counter.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    });
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should increase number when call OnIncrease', ()=>{
+    component.number = 2
+    component.OnIncrease()
+    expect(component.number).toEqual(3)
+  })
+
+  it('should dncrease number when call OnDecrease', ()=>{
+    component.number = 2
+    component.OnDecrease()
+    expect(component.number).toEqual(1)
+  })
+});
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  number = 0
+  OnIncrease(){
+    this.number ++
+  }
+  OnDecrease(){
+    this.number --
+  }
+}
